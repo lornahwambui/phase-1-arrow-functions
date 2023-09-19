bae269194b4cccd21cccbafbd3e0e6c335583583
lornahWambuiGitau <lornah.gitau@student.moringaschool.com>
2023-09-19 23:28:31 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+const divide = ()=>{
+    return 2000/100
+};
+const result1 =divide();
+console.log(result1)
+
+const square = (x) =>{
+    return x * x;
+};
+const x=2;
+const result =square(x);
+console.log(result);
+
+
+const add = (a, b) => {
+    return a + b;
+  };
+  const a = 3;
+  const b = 4;
+  const result2= add(a, b);
+  console.log(result2);
